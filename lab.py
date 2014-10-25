a4499e93f5fad4f41b4c203e3310c3424491c9ff
Gokhan Baris Aker <gokhanbarisaker@gmail.com>
2014-10-25 20:41:01 +0400

    Added html template renderer and dummy html

--- lab.py
+++ lab.py
@@ -1,7 +1,8 @@
 # -*- coding: UTF-8 -*-
 
 import webapp2
+import html
 
 class Handler(webapp2.RequestHandler):
   def get(self):
-    self.response.out.write("Hellow")
+    self.response.out.write(html.render('lab.html'))
